f23b67e309e45599232ea5f8045ee3e26a60fa02
Logan Weigel <snr.wiggels@gmail.com>
2024-07-02 15:31:28 -0500

    Create models and build db

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -2,10 +2,32 @@ using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace pet_hotel.Models;
-
+public enum PetBreed
+{
+   Shepherd, Poodle, Beagle, Bulldog, Terrier, Boxer, Labrador, Retriever,
+}
+public enum PetColor
+{
+   White, Black, Golden, Tricolor, Spotted
+}
 public class Pet
 {
-   // Fields go here.
-   // Hint: nullable types are a thing in c#, you will need to google
-   // this so that `checkedInAt` can accept `null` as a value
+   public int Id { get; set; }
+   [Required]
+
+   public string Name { get; set; }
+   [Required]
+
+   public PetColor Color { get; set; }
+   [Required]
+
+   public DateTime? CheckedInAt { get; set; }
+
+   [Required]
+   public PetBreed Breed { get; set; }
+
+   [Required]
+   [JsonConverter(typeof(JsonStringEnumConverter))]
+   public int PetOwnerid { get; set; }
+
 }
--- Models/PetOwner.cs
+++ Models/PetOwner.cs
@@ -6,5 +6,24 @@ namespace pet_hotel.Models;
 
 public class PetOwner
 {
-   // Fields go here
+   public int Id { get; set; }
+
+   [Required]
+   public string Name { get; set; }
+
+   [Required]
+   [EmailAddress]
+   public string Email { get; set; }
+
+   [JsonIgnore]
+   public List<Pet> Pets { get; set; }
+
+   [NotMapped]
+   public int PetCount
+   {
+      get
+      {
+         return (Pets != null) ? Pets.Count : 0;
+      }
+   }
 }
